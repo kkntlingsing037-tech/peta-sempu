04d9d061c52eec323f17a24d6b63b50f3a829668
unknown <muhzaki46@gmail.com>
2026-07-23 21:42:27 +0700

    Fix satellite map tile unavailable text at high zoom levels using maxNativeZoom: 17

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const basemaps = {
       osm: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, crossOrigin: true }),
-      satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', { maxZoom: 19, crossOrigin: true }),
+      satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', { maxZoom: 19, maxNativeZoom: 17, crossOrigin: true }),
       dark: L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', { maxZoom: 19, crossOrigin: true })
     };
 
